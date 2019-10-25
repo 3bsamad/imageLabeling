4f697b17ad979e5bce916ef96a89f3aa1d59290b
Omar M. Abdel Samad <52745754+3bsamad@users.noreply.github.com>
2019-10-25 17:56:00 +0200

    Update and rename test.py to changeMask.py

--- changeMask.py
+++ changeMask.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 16:40:33 2019
 
-@author: ctj-oe
 """
-
+Created on Fri Oct 25 17:53:30 2019
+@author: Omar M. Abdel Samad
+"""
+#import the required packages
 import cv2
 import numpy as np
 from PIL import Image
-src1 = cv2.imread("img1.png")
-src2 = cv2.imread("red.png")
+
+#the mask already created with only black and white 
 mask= Image.open("label1.png")
-
-
 
 
 img = mask.convert("RGBA")
@@ -21,11 +18,10 @@ for y in range(img.size[1]):
     for x in range(img.size[0]):
         r, g, b, a = img.getpixel((x, y))
         if (r ==255) and (g ==255) and (b ==255):
-            pixdata[x, y] = (150, 0, 0, 500) #black background
+            pixdata[x, y] = (150, 0, 0, 500) #red color of value 150 
         if r == 0 and g == 0 and b == 0:
             pixdata[x, y] = (0, 0, 0, 1000)
-            
-            
+                       
 img.save("redmask.png")
 
 
